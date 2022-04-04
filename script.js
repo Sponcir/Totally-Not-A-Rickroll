c97784c2169c0331503bd5925e7048cd075cfab5
Sponcir <97147938+Sponcir@users.noreply.github.com>
2022-04-05 09:07:27 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const you = {
         console.log("click here ----> https://www.youtube.com/watch?v=dQw4w9WgXcQ");
     },
 };
+const yousuck = {
+    penis() {
+        console.log("No I Don't ----> https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    },
+};
